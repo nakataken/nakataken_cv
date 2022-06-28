10763440ac36edb02e34db2792c79364a7aaad81
nakataken <ken.nakata130@gmail.com>
2022-06-28 21:30:25 +0800

    Add animation and title

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,8 +15,11 @@ const Home = () => {
           <div className="bg-[#FCC5C0] absolute bottom-50 left-[335px] h-60 w-1"></div>
         </div>
         <p className="text-[#FCC5C0]">Hi, my name is</p>
-        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
-          Ken Nakata
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6] group">
+          <span className="inline-flex h-20 pt-2 overflow-y-hidden overflow-x-hidden animate-type group-hover:animate-type-reverse whitespace-nowrap text-brand-accent will-change-transform">
+            Ken Nakata
+          </span>
+          <span className="box-border inline-block w-1 h-10 ml-2 -mb-2 bg-white md:-mb-4 md:h-16 animate-cursor will-change-transform"></span>
         </h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
           I'm a Aspiring Developer
